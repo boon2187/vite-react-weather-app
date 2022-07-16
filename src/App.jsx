80f7e0070335ca4868510d94a90187590ff15280
boon2187 <fumiya.mai.ito@gmail.com>
2022-07-16 10:21:32 +0900

    Formにあった都市名のstate(city)を上位コンポーネントのAppに移動。各コンポーネントで共有できるようにした。

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import './App.css'
+import { useState } from 'react';
+import axios from 'axios';
+
 import Title from './components/Title.jsx'
 import Form from './components/Form.jsx'
 import Results from './components/Results'
 
+import './App.css'
+
+
 function App() {
+  // Formから持ってきた元「FormのデータをAPIに送るためにstateに取っておく」部分
+  const [city ,setCity] =useState("");
+
+  // Formから思ってきた元「axiosを使ってAPIに都市名を送る」部分
+  const getWeather = (e) => {
+      e.preventDefault();
+      axios.get("https://api.weatherapi.com/v1/current.json?key=0be11468eae249b0b82234208221507&q=London&aqi=no").then(res => console.log(res))
+
+  }
 
   return (
     <div className="App">
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,22 +1,11 @@
 // Form.jsx
 
-import { useState } from 'react';
-import axios from 'axios';
-
 // 都市名LondonでもらったURLを、テスト用にとりあえず保存しておく
 // "https://api.weatherapi.com/v1/current.json?key=0be11468eae249b0b82234208221507&q=London&aqi=no"
 
 const Form = () => {
 
-    // FormのデータをAPIに送るためにstateに取っておく
-    const [city ,setCity] =useState("");
 
-    // axiosを使ってAPIに都市名を送る
-    const getWeather = (e) => {
-        e.preventDefault();
-        axios.get("https://api.weatherapi.com/v1/current.json?key=0be11468eae249b0b82234208221507&q=London&aqi=no").then(res => console.log(res))
-
-    }
     return (
         <form>
             <input type="text" name="city" placeholder="都市名" onChange={ e => setCity(e.target.value) } />
